511eae0a3ac38ede016765bbb44f5cb48d5adf2b
IN1DM3_09 <IN1DM3_09@HZ317738.H015112.NET>
2019-01-21 10:48:53 +0100

    Actualizacion con las clases de Datos y Ficheros creadas

--- Reto3_GrupoB/src/modelo/Datos.java
+++ Reto3_GrupoB/src/modelo/Datos.java
@@ -1,7 +1,11 @@
 package modelo;
 
-public class datos {
+public class Datos {
 
-	static String pass;
+	static String pass_BBDD="";
+	static String user_BBDD="";
+	static String URL_Fichero="";
+	static String URL_destino_ticet="";
+	
 	
 }
---
+++ Reto3_GrupoB/src/modelo/Ficheros.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Ficheros {
+	
+	
+	//Metodo para cargar el fichero con los datos necesarios
+	public static void cargarFichero() {
+		
+	}
+	
+	//Metodo para guardar los datos del fichero en un array
+	
+	//Metodo para guardar los datos del ticket en un fichero
+
+}
