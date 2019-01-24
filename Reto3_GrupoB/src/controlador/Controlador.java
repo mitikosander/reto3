1a9efbcd1b713fdf10f12c35476793ec27aa5c01
mañana <maana@ORD37>
2019-01-24 10:42:32 +0100

    (Casi)Todos los putos botones estan funcionando.
     Faltan los de los billetes

--- Reto3_GrupoB/src/controlador/Controlador.java
+++ Reto3_GrupoB/src/controlador/Controlador.java
@@ -25,80 +25,119 @@ public class Controlador {
 	
 	private void InitializeEvents() {
 	
-		//Añadimos la funcion que cambia de pantalla al pulsar el boton login
+		//Te mete a la pantalla login
 		vista.pantCarga.btnAccederInicio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				vista.mostrarPantalla(vista.login);
 			}
 		});
+		//Cuando estas en la pantalla login, te lleva a la pantalla de carga
+		vista.login.btnCancelarLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.pantCarga);
+			}
+		});
 		
+		//Te lleva a la pantalla de registro
 		vista.pantCarga.btnRegistrarseInicio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				vista.mostrarPantalla(vista.registro);
 			}
 		});
+		//Te lleva a la pantalla de carga
 		vista.registro.btnCancelarRegistro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);
+				vista.mostrarPantalla(vista.pantCarga);
 			}
 		});
+		
+		//Te lleva a la pantalla de carga, habiendo creado el usuario
 		vista.registro.btnAceptarRegistro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
+		
+		//Te lleva a la pantalla de login
 		vista.pantCarga.btnAccederInicio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.login);			}
 		});
+		//Te lleva a la pantalla de lineas
+		vista.login.btnAceptarLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.lineas);			}
+		});
+		
 		vista.lineas.btnLineasLinea1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.paradas);			}
 		});
 		vista.lineas.btnLineasLinea2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.paradas);			}
 		});
 		vista.lineas.btnLineasLinea3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.paradas);			}
 		});
 		vista.lineas.btnLineasLinea4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.paradas);			}
 		});
+		
 		vista.lineas.btnCancelarLineas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
+		vista.seleccionFecha.btnCancelarSeleccionFecha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.pantCarga);			}
+		});
+		vista.seleccionFecha.btnAceptarSeleccionFecha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.pantCarga);			}
+		});
+		
 		vista.paradas.btnAceptarParadas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.ticket);			}
 		});
 		vista.paradas.btnCancelarParadas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
-		});
-		vista.ticket.btnCancelarTicket.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
 		vista.ticket.btnPagarTicket.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pagar);			}
+		});
+		vista.ticket.btnCancelarTicket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.pantCarga);			}
+		});
+		
+		
+		
+		vista.ticket.btnCancelarTicket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.pantCarga);			}
+		});
+		vista.ticket.btnPagarTicket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vista.mostrarPantalla(vista.ticket);			}
 		});
 		vista.pagar.btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
 		vista.pagar.btnFinalizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Falta un metodo que borre todos los datos al volver al inicio
-				vista.mostrarPantalla(vista.registro);			}
+				
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
 		vista.pagar.btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//Guarda los datos en un archivo y vuelve al inicio 
-				vista.mostrarPantalla(vista.registro);			}
+				vista.mostrarPantalla(vista.pantCarga);			}
 		});
 		
 		
