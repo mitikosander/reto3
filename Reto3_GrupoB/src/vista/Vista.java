d4fc9ec283af6757bd5b286c40678359e539683b
in1dm3b_09 <in1dm3b_09@HZ317738.H015112.NET>
2019-01-25 09:55:59 +0100

    Implementación de metodos para poner datos en los labels

--- Reto3_GrupoB/src/vista/Vista.java
+++ Reto3_GrupoB/src/vista/Vista.java
@@ -1,5 +1,6 @@
 package vista;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Vista {
@@ -30,9 +31,24 @@ public class Vista {
 		
 		
 	}
+	//método para 
 	public  void mostrarPantalla(JPanel panel) {
 		ventana.setContentPane(panel);
 		ventana.setVisible(true);
 		
+	}
+	
+	//método para poner un String en un label
+	public void setStringLabel(JLabel etiqueta, String txt) {
+		etiqueta.setText(txt);
+	}
+	//método para poner double en un label que le pasemos
+	public void setDoubleLabel(JLabel etiqueta, double d) {
+		etiqueta.setText(Double.toString(d));
+	}
+	
+	//método para poner ints en un label que le pasemos
+	public void setIntLabel(JLabel etiqueta, int n) {
+		etiqueta.setText(Integer.toString(n));
 	}
 }
